cff05ea469672b2ec6f4112b81add31dc1873d08
Maulik <patnimaulik96@gmail.com>
2017-10-08 22:39:37 +0530

    User crawl in chunks

--- src/crawl_users.py
+++ src/crawl_users.py
@@ -1,17 +1,32 @@
 import json
+import sys
 from constants import *
 
 
 def crawlUserData(users):
+    print("Crawling user data...")
+    with open(USER_DATAFILE, "w") as f:
+        json.dump({}, f)
     data = {}
+    count = 0
+    n = len(users)
     for user in users:
         following = []
+        count += 1
         for friend in twarc.friend_ids(user):
             if friend in users:
                 following.append(friend)
         data[user] = following
-    with open(USER_DATAFILE, "w") as f:
-        json.dump(data,f)
+        if count == n or count%10 == 0:
+            with open(USER_DATAFILE) as f:
+                storedData = json.load(f)
+            storedData.update(data)
+            with open(USER_DATAFILE, 'w') as f:
+                json.dump(storedData, f)
+            data = {}
+            sys.stdout.write('\r')
+            sys.stdout.write('{} users crawled. {}% done'.format(count, (count*100.0)/n))
+            sys.stdout.flush()
 
 if __name__ == '__main__':
 
