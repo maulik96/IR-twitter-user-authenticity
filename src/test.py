be7bc09411be326649bc0a32aaa7df3c00d09171
Maulik <patnimaulik96@gmail.com>
2017-11-13 20:54:28 +0530

    Modify test.py to make graph

--- src/test.py
+++ src/test.py
@@ -7,8 +7,8 @@ import matplotlib.pyplot as plt
 def checkForVerifiedUsers():
     with open(RATINGS_FILE) as f:
         ratings = json.load(f)
-    # with open(VERIFIED_USERS) as f:
-    #     users = json.load(f)
+    with open(VERIFIED_USERS) as f:
+        users = json.load(f)
     
     k = 25
     d = Counter(ratings)
@@ -37,15 +37,20 @@ def generateTopUserGraphs():
     with open(TOPUSERS_FILE) as f:
         data = json.load(f)
     i = 0
-    pts = []
+    pts = [[],[]]
     total = 0.0
     for u in data:
         i += 1
         total += u[1]
         if i%5 == 0:
             avg = total/i
-            pts.append([i, avg])
-    plt.plot(pts)
+            pts[0].append(i)
+            pts[1].append(avg)
+    print(pts)
+    line2, = plt.plot(pts[0], pts[1], '-o', label='Our method')
+    pts = [[5, 10, 15, 20, 25], [0.3811433262989935, 0.24236761097017653, 0.19182337651553366, 0.1579906961936422, 0.13558378660579276]]
+    line1, = plt.plot(pts[0], pts[1], '-o', label='TU rank')
+    plt.legend(handles=[line1, line2])
     plt.title("Average Adequacy of Top k Users")
     plt.ylabel('Adequacy')
     plt.xlabel('Top k')
