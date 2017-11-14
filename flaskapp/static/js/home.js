ec795431ed51c3d58cf689e42377f691a316a116
Sambhav Kothari <sambhavs.email@gmail.com>
2017-11-14 14:21:36 +0530

    Use handle as div id

--- flaskapp/static/js/home.js
+++ flaskapp/static/js/home.js
@@ -1,8 +1,8 @@
 function tagUser (id, handle, value) {
-    $("#"+id).removeClass('untagged').removeClass('no').removeClass('yes').removeClass('unsure');
-    $("#"+id).addClass('pending');
+    $("#"+handle).removeClass('untagged').removeClass('no').removeClass('yes').removeClass('unsure');
+    $("#"+handle).addClass('pending');
     $.post("/tag/", {"user_handle": handle, "tag_value": value}, function(data, textStatus, xhr) {
-        $("#"+id).removeClass('pending').addClass(value);
+        $("#"+handle).removeClass('pending').addClass(value);
         console.log(data);
     });
 }
